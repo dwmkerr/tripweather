639edef765228ef6d985a8de5833b868e1a4d383
Dave Kerr <dwmkerr@gmail.com>
2024-02-24 06:43:17 +0000

    chore: finish mapping pirate weather

--- functions/src/pirate-weather.ts
+++ functions/src/pirate-weather.ts
@@ -49,6 +49,24 @@ export type PirateWeatherIcon =
   | "partly-cloudy-day"
   | "partly-cloudy-night";
 
+export interface PirateWeatherAlert {
+  title: string; // "Wind Advisory issued January 24 at 9:25AM CST until January 24 at 6:00PM CST by NWS Corpus Christi TX"
+  regions: string[]; // ["Live Oak", " Bee", " Goliad", " Victoria", " Jim Wells", " Inland Kleberg", " Inland Nueces", " Inland San Patricio", " Coastal Aransas", " Inland Refugio", " Inland Calhoun", " Coastal Kleberg", " Coastal Nueces", " Coastal San Patricio", " Aransas Islands", " Coastal Refugio", " Coastal Calhoun", " Kleberg Islands", " Nueces Islands", " Calhoun Islands"]
+  severity: string; // "Moderate"
+  time: number; // 1674573900
+  expires: number; // 1674604800
+  description: string; // "* WHAT...Southwest winds 25 to 30 mph with gusts up to 40 mph.  * WHERE...Portions of South Texas.  * WHEN...Until 6 PM CST this evening.  * IMPACTS...Gusty winds could blow around unsecured objects. Tree limbs could be blown down and a few power outages may result."
+  uri: string; // "https://api.weather.gov/alerts/urn:oid:2.49.0.1.840.0.492c55233ef16d7a98a3337298c828b0f358ea34.001.1
+}
+
+export interface PirateWeatherSourceTimes {
+  "hrrr_0-18": string; // "2023-01-21 14:00:00"
+  hrrr_subh: string; // "2023-01-21 14:00:00"
+  "hrrr_18-48": string; // "2023-01-21 12:00:00"
+  gfs: string; // "2023-01-21 06:00:00"
+  gefs: string; // "2023-01-21 06:00:00
+}
+
 export interface PirateWeatherResponse {
   latitude: number; // 45.42
   longitude: number; // -75.69,
@@ -71,35 +89,11 @@ export interface PirateWeatherResponse {
     icon: PirateWeatherIcon; // "cloudy"
     data: PirateWeatherData[];
   };
-  alerts: any;
-  //  "alerts": [
-  //     {
-  //         "title": "Wind Advisory issued January 24 at 9:25AM CST until January 24 at 6:00PM CST by NWS Corpus Christi TX",
-  //         "regions": ["Live Oak", " Bee", " Goliad", " Victoria", " Jim Wells", " Inland Kleberg", " Inland Nueces", " Inland San Patricio", " Coastal Aransas", " Inland Refugio", " Inland Calhoun", " Coastal Kleberg", " Coastal Nueces", " Coastal San Patricio", " Aransas Islands", " Coastal Refugio", " Coastal Calhoun", " Kleberg Islands", " Nueces Islands", " Calhoun Islands"],
-  //         "severity": "Moderate",
-  //         "time": 1674573900,
-  //         "expires": 1674604800,
-  //         "description": "* WHAT...Southwest winds 25 to 30 mph with gusts up to 40 mph.  * WHERE...Portions of South Texas.  * WHEN...Until 6 PM CST this evening.  * IMPACTS...Gusty winds could blow around unsecured objects. Tree limbs could be blown down and a few power outages may result.",
-  //         "uri": "https://api.weather.gov/alerts/urn:oid:2.49.0.1.840.0.492c55233ef16d7a98a3337298c828b0f358ea34.001.1"
-  //     },
-  // ]
-  flags: any;
-  // "flags": {
-  //       "sources": [
-  //          "ETOPO1",
-  //          "gfs",
-  //          "gefs",
-  //          "hrrrsubh",
-  //          "hrrr"
-  //     ],
-  sourceTimes: any;
-  // "sourceTimes": {
-  //        "hrrr_0-18": "2023-01-21 14:00:00",
-  //        "hrrr_subh": "2023-01-21 14:00:00",
-  //        "hrrr_18-48": "2023-01-21 12:00:00",
-  //        "gfs": "2023-01-21 06:00:00",
-  //        "gefs": "2023-01-21 06:00:00"
-  //   },
+  alerts: PirateWeatherAlert[];
+  flags: {
+    sources: string[]; // [ "ETOPO1", "gfs", "gefs", "hrrrsubh", "hrrr" ]
+  };
+  sourceTimes: PirateWeatherSourceTimes;
   "nearest-station": number; // 0
   units: string; // "ca"
   version: string; // "V1.5.5"
@@ -135,7 +129,6 @@ export const weather = onCall<WeatherRequest, Promise<WeatherResponse>>(
 
       //  Get the data as a weather response and return to the caller.
       const weatherResponse = data as PirateWeatherResponse;
-      logger.debug("Received weather", weatherResponse);
       logger.debug(
         `Summary: ${weatherResponse.currently.summary} (${weatherResponse.currently.icon})`,
       );
